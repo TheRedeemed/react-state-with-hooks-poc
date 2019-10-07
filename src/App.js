a481b9530c217d0e78b3fb5a3d42956312760e5d
TheRedeemed <abdoulkaders77@gmail.com>
2019-10-07 08:17:30 -0400

    manage state with useState for input field submit button and checkbox

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import uuid from 'uuid/v4'
+
+const initialTodos = [
+  {
+    id: uuid(),
+    task: 'Learn React',
+    complete: true
+  },
+  {
+    id: uuid(),
+    task: 'Learn Firebase',
+    complete: false
+  },
+  {
+    id: uuid(),
+    task: 'Learn GraphQL',
+    complete: false
+  }
+];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos)
+  const [task, setTask] = useState('')
+
+  const handleChangeInput = event => setTask(event.target.value)
+
+  const handleChangeCheckbox = id => setTodos(todos.map( todo => (todo.id === id) ? { ...todo, complete: !todo.complete } : todo ))
+
+
+  const handleSubmit = event => {
+    if (task) setTodos([...todos, {id: uuid(), task: task,complete: false}])
+    setTask('')
+    event.preventDefault()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {
+          todos.map(todo => (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type='checkbox'
+                  checked={todo.complete}
+                  onChange={() => handleChangeCheckbox(todo.id)}
+                />
+                {todo.task}
+              </label>
+            </li>
+          ))
+        }
+      </ul>    
+
+      <form onSubmit={handleSubmit}>
+        <input 
+          type='text' 
+          value={task} 
+          onChange={handleChangeInput} 
+        />
+        <button type='submit'>Add Todo</button>
+      </form>
     </div>
   );
 }
